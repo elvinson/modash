385f37edaa86d74d6e9b408a7c60ce0d67df6a41
elvinson <elvinmd@gmail.com>
2022-02-13 19:13:43 +0300

    init

---
+++ src/function/delay.js
@@ -0,0 +1,14 @@
+/**
+ * Invokes func after wait milliseconds. 
+ * Any additional arguments are provided to func when it's invoked.
+ * @async
+ * @param {Function} func - The function to delay.
+ * @param {number} wait - The number of milliseconds to delay invocation.
+ * @param {Array} args - The arguments to invoke func with.
+ * @returns {number} Returns the timer id.
+ */
+const delay = function(func, wait, args) {
+    
+};
+
+module.export = delay;
---
+++ test/function/delay.js
@@ -0,0 +1,8 @@
+const delay = require('../../src/function/delay');
+
+QUnit.module('function: delay');
+
+QUnit.test('#1', function(assert) {
+    
+});
+
